e35181ce5a664f16372679a290041697670640ec
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-04-28 19:49:46 +0700

    add back navigation at city detail pag

--- resources/views/pages/city/detail.blade.php
+++ resources/views/pages/city/detail.blade.php
@@ -3,6 +3,17 @@
         {{$city->name}}
     </x-slot:title>
 
+    <a href="/" class="inline-flex items-center gap-2 text-md hover:underline mb-8">
+        <span class="shrink-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left">
+                <path d="m12 19-7-7 7-7" />
+                <path d="M19 12H5" />
+            </svg>
+        </span>
+        <span>Back</span>
+    </a>
+
+
 
     <div class="flex gap-x-8">
         <div class="card bg-base-100 w-96 shadow-sm max-h-67">
